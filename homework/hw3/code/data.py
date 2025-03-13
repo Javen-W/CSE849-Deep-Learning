bf00a82523973b55fadde59f814342ca51c66494
Javen William Zamojcin <javen.william@gmail.com>
2025-03-13 15:47:33 -0400

    impl data preprocessing

--- homework/hw3/code/data.py
+++ homework/hw3/code/data.py
@@ -1,20 +1,70 @@
+import os
+
 import torch
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
 from torchvision.datasets import ImageFolder
 from torchvision.transforms import v2
 
 torch.manual_seed(123)
 
+DATASET_ROOT = '/home/javen/Documents/Datasets/CSE849-HW3/'
+NORMAL_MEAN = [0.438, 0.435, 0.422]
+NORMAL_STD = [0.228, 0.225, 0.231]
+
+"""
+class TestDataset(Dataset):
+    def __init__(self, x, y):
+        super().__init__()
+        self.x = x
+        self.y = y
+
+    def __len__(self):
+        return len(self.x)
+
+    def __getitem__(self, index):
+        return self.x[index].reshape((1,)), self.y[index].reshape((1,))
+"""
 
 def create_dataloaders():
-    train_tf = None # TODO: Define the train transform
-    val_tf = None # TODO: Define the validation transform. No random augmentations here.
+    # TODO: Define the training transform
+    train_tf = v2.Compose([
+        v2.RandomHorizontalFlip(),
+        v2.PILToTensor(),
+        v2.Normalize(mean=NORMAL_MEAN, std=NORMAL_STD),
+        v2.RandomErasing(),
+    ])
 
-    train_dataset = None # TODO: Load the train dataset. Make sure to pass train_tf to it.
-    val_dataset = None # TODO: Load the val dataset.
+    # TODO: Define the validation transform. No random augmentations here.
+    val_tf = v2.Compose([
+        v2.PILToTensor(),
+        v2.Normalize(mean=NORMAL_MEAN, std=NORMAL_STD),
+    ])
 
-    train_loader = None # TODO: Create the train dataloader
-    val_loader = None # TODO: Create the val dataloader
+    # TODO: Load the train dataset. Make sure to pass train_tf to it.
+    train_dataset = ImageFolder(
+        root=os.path.join(DATASET_ROOT, 'train'),
+        transform=train_tf,
+    )
+
+    # TODO: Load the val dataset.
+    val_dataset = ImageFolder(
+        root=os.path.join(DATASET_ROOT, 'val'),
+        transform=val_tf,
+    )
+
+    # TODO: Create the train dataloader
+    train_loader = DataLoader(
+        dataset=train_dataset,
+        batch_size=64,
+        shuffle=True,
+    )
+
+    # TODO: Create the val dataloader
+    val_loader = DataLoader(
+        dataset=val_dataset,
+        batch_size=64,
+        shuffle=False,
+    )
 
     return train_loader, val_loader
 
