92dd73b933f27fd1d1d06cb76ebdf8e8d78d7ddd
Javen William Zamojcin <javen.william@gmail.com>
2025-03-19 19:32:30 -0400

    impl q2

--- homework/hw3/code/q2.py
+++ homework/hw3/code/q2.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 import imageio.v2 as imio
 import os
@@ -13,12 +14,24 @@ model.load_state_dict(torch.load("results/q1_model.pt", weights_only=True))
 
 model.eval()
 
-conv_weights = None # Get the conv1 layer weights
+conv_weights = model.conv1.weight # Get the conv1 layer weights
 
 os.makedirs("q2_filters", exist_ok=True)
 
 for i in range(conv_weights.shape[0]):
-    f = None # get the i-th filter
-    # TODO: Normalize the filter to [0, 255] as convert it to uint8.
-    # Otherwise, it will not be visualized correctly.
+    # Get the i-th filter - shape will be [3, 7, 7]
+    f = conv_weights[i].detach().cpu().numpy()
+
+    # Take mean across channels to get 7x7 filter
+    f = np.mean(f, axis=0)
+
+    # Normalize to [0, 255]
+    f_min, f_max = f.min(), f.max()
+    if f_max > f_min:  # Avoid division by zero
+        f = (f - f_min) / (f_max - f_min)  # Scale to [0, 1]
+        f = (f * 255).astype(np.uint8)  # Convert to uint8
+    else:
+        f = np.zeros_like(f, dtype=np.uint8)  # If filter is constant, make it zero
+
+    # Save using imageio
     imio.imwrite(f"q2_filters/filter_{i}.png", f)
