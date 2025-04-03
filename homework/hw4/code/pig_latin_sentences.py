015179211c10d037326a7a91356d98aa64a90f96
Javen <javen.william@gmail.com>
2025-04-03 15:30:30 -0400

    impl pig_latin_sentences.py

--- homework/hw4/code/pig_latin_sentences.py
+++ homework/hw4/code/pig_latin_sentences.py
@@ -6,16 +6,34 @@ class PigLatinSentences(torch.utils.data.Dataset):
         self.char_to_idx = char_to_idx
         self.english_sentences = []
         self.pig_latin_sentences = []
+        self.split = split
+        self.sos = [self.char_to_idx['<sos>']]
+        self.eos = [self.char_to_idx['<eos>']]
 
-        # TODO: Load the data from the file to self.english_sentences
-        # and self.pig_latin_sentences
+        # TODO: Load the data from the file to self.english_sentences and self.pig_latin_sentences
+        # Load the appropriate text file based on the split
+        data_file = f'code/data/reviews_pig_latin_data_{self.split}.txt'
+        with open(data_file, 'r', encoding='utf-8') as f:
+            for line in f:
+                # Skip empty lines
+                if not line.strip():
+                    continue
+                # Parse each line as a separate JSON object
+                item = json.loads(line.strip())
+                self.english_sentences.append(item['original'])
+                if split != 'test':
+                    self.pig_latin_sentences.append(item['pig_latin'])
 
     def __len__(self):
         return len(self.english_sentences)
 
     def __getitem__(self, idx):
         # TODO: Load corresponding english and pig latin sentences,
-        # append <sos> and <eos> tokens, convert them to indices using
-        # char_to_idx, and return the indices.
-        return eng_word_idx, pig_latin_word_idx
-        
+        # append <sos> and <eos> tokens, convert them to indices using char_to_idx, and return the indices.
+        eng_word_idx = torch.tensor(self.sos + [self.char_to_idx[c] for c in self.english_sentences[idx]] + self.eos)
+        if self.split == "test":
+            return eng_word_idx
+        else:
+            pig_latin_word_idx = torch.tensor(
+                self.sos + [self.char_to_idx[c] for c in self.pig_latin_sentences[idx]] + self.eos)
+            return eng_word_idx, pig_latin_word_idx
