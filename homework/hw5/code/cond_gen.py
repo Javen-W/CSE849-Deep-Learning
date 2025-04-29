2e0c2ccfa21038338cdbbf73eb5bf0a98e41f33a
Javen William Zamojcin <javen.william@gmail.com>
2025-04-28 20:12:58 -0400

    fix cond_gen sampling

--- homework/hw5/code/cond_gen.py
+++ homework/hw5/code/cond_gen.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 import numpy as np
 import matplotlib.pyplot as plt
 plt.switch_backend("agg")
-
+from tqdm import trange, tqdm
 from models import MLP
 from data import States
 
@@ -25,7 +25,7 @@ n_steps = 500
 classifier = MLP(input_dim=3, output_dim=5, hidden_layers=[100, 200, 500]).to(device)
 classifier.load_state_dict(torch.load(os.path.join(checkpoints_dir, "classifier.pt"), weights_only=True))
 classifier.eval()
-logsoftmax = nn.LogSoftmax() # create log-softmax
+logsoftmax = nn.LogSoftmax(dim=0) # create log-softmax
 
 # create your denoiser model architecture and load the weights from uncond_gen.py
 denoiser = MLP(input_dim=3, output_dim=2, hidden_layers=[256, 256, 256, 256]).to(device)
@@ -44,13 +44,14 @@ def sample(label, num_samples=1000):
         eps = denoiser(z_) # get the denoiser prediction
 
         # compute the gradient of the log-softmax classifier w.r.t. the input.
-        cls_logits = logsoftmax(classifier(z_))[label]
-        cls_grad = cls_logits.backward()
+        cls_grad = logsoftmax(classifier(z_))[:, label]
+        cls_grad.backward(gradient=torch.ones_like(cls_grad), retain_graph=True)
+        cls_grad = cls_grad.reshape(-1, 1)
 
         # Sampling step
         alpha_bar_t = dataset.alpha_bar[i].to(device)
         alpha_bar_tm1 = dataset.alpha_bar[i - 1].to(device)
-        eps_hat = eps - torch.sqrt(1 - alpha_bar_t) * cls_grad * cls_logits
+        eps_hat = eps - torch.sqrt(1 - alpha_bar_t) * cls_grad
         z = torch.sqrt(alpha_bar_tm1) * ((z - torch.sqrt(1 - alpha_bar_t) * eps_hat) / torch.sqrt(alpha_bar_t)) + torch.sqrt(1 - alpha_bar_tm1) * eps_hat
  
     z = z.detach().cpu().numpy()
@@ -59,7 +60,7 @@ def sample(label, num_samples=1000):
 
 for label in range(5):
     full_z = []
-    for i in range(5):
+    for i in trange(5):
         nll, z = sample(label)
         full_z.append(z)
     full_z = np.concatenate(full_z, axis=0)
